61d5ca092ee838def386534d0f2de16ba1dd6cff
Omulosi <mulongojohnpaul@gmail.com>
2021-11-26 16:47:12 +0300

    Add edit user profile view and functionality

--- microblog/routes.py
+++ microblog/routes.py
@@ -1,11 +1,11 @@
 """
-Routes
+Views
 """
 from datetime import datetime
 from flask import request, render_template, flash, redirect, url_for, Blueprint
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from microblog.forms import LoginForm, RegistrationForm
+from microblog.forms import LoginForm, RegistrationForm, EditProfileForm
 from microblog.models import User
 from microblog.extensions import db
 
@@ -66,6 +66,23 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('user.login'))
     return render_template('register.html', title='Register', form=form)
+
+@bp.route('edit_profile')
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    
+    if form.validate_on_submit():
+        current_user.about_me = form.about_me.data
+        current_user.username = form.username.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user.edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    
+    return render_template('edit_profile.html', form=form, title='Edit Profile')
 
 @bp.route('/user/<username>')
 @login_required
